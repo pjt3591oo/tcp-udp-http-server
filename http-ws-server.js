d5c67754fb6f3a1e05104e9f08955fc0f3cae7ce
pjt3591oo <pjt3591oo@gmail.com>
2022-01-26 23:58:55 +0900

    http-ws-server.js encode, decode 추가

--- http-ws-server.js
+++ http-ws-server.js
@@ -16,11 +16,82 @@ function handshakeMsg(msg) {
          '\r\n';
 }
 
-const server = http.createServer(function (req, res) {});
+function convertToBinary1 (number) {
+  let num = number;
+  let binary = (num % 2).toString();
+  for (; num > 1; ) {
+      num = parseInt(num / 2);
+      binary =  (num % 2) + (binary);
+  }
+  return binary;
+}
+
+function decode(data) {
+  let FIN = data[0] >> 7 == 1;
+  let RSV1 = (data[0] >> 6 & 1) == 1;
+  let RSV2 = (data[0] >> 5 & 1) == 1;
+  let RSV3 = (data[0] >> 4 & 1) == 1;
+  let OPCODE = data[0] & 0xf;
+
+  console.log('FIN, RSV1, RSV2, RSV3, OPCODE', convertToBinary1(data[0]))
+  console.log(FIN, RSV1, RSV2, RSV3, OPCODE)
+  console.log()
+  
+  const IS_MASK = data[1] >> 7 == 1;
+  const PAYLOAD_LENGTH = data[1] & 0x7f;
+
+  console.log(`IS_MASK: ${IS_MASK}, PAYLOAD_LENGTH: ${PAYLOAD_LENGTH}`)
+
+  const MASK = data.slice(2, 6);
+  const payload = data.slice(8);
+  console.log('mask', MASK)
+  console.log('payload', payload)
+   
+  const buffer = Buffer.alloc(PAYLOAD_LENGTH); //  payload 저장용 버퍼생성
+
+  for (let i = 0 ; i < PAYLOAD_LENGTH ; i++) {
+    buffer[i] = data[6 + i] ^ MASK[i % 4]
+  }
+
+  return buffer.toString('utf-8')
+}
+
+function encode(msg) {
+  const RES_PAYLOAD_LENGTH = msg.length.toString(2).padStart(8, 0); // 2진수 변환
+
+  const responseBuffer =  Buffer.concat([
+    // FIN(1), RSV1(0), RSV2(0), RSV3(0), OPCODE(0001) 
+    // 10000001 => 81
+    Buffer.from('81'.toString(16), 'hex'),
+    // mask + payload_length 서버 -> 클라이언트 시 mask는 0이므로 payload 길이만 가지고 만든다.
+    Buffer.from(parseInt(RES_PAYLOAD_LENGTH, 2).toString(16).padStart(2, 0), 'hex'), 
+    // 페이로드 메시지 인코딩(UTF-8)
+    Buffer.from(msg)
+  ])
+
+  return responseBuffer;
+}
+
+const server = http.createServer(function (req, res) {
+  console.log('123')
+});
 
 server.on("upgrade", function (req, socket, head) {
+  console.log(req.headers)
   let headersReturn = handshakeMsg(req.headers["sec-websocket-key"])
   socket.write(headersReturn);
+
+  socket.on('data', (data) => {
+    console.log('========= dataframe parse start =========')
+    console.log(data)
+    console.log('decode: ', decode(data));
+    const msgBuffer = encode('abcdefghijk');
+
+    console.log('encode: ', msgBuffer);
+    socket.write(msgBuffer)
+    socket.write(msgBuffer)
+    console.log('========= dataframe parse end =========')
+  })
 });
 
 server.listen(5000, function () {
